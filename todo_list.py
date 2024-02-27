1f4783024572f89333018de16b077e7b4f4e4c6b
AHS CS <ahs-cs@ausdk12.org>
2024-02-27 10:04:01 -0800

    oh my god numbers

--- todo_list.py
+++ todo_list.py
@@ -11,14 +11,47 @@
 # Stores items in CSV which gets edited(?)
 # figure out how to change the text on the editor without new line
 
-import csv
 
-with open("todo_list.csv", mode='r+') as todo_list_file:
-    todo_list = csv.DictReader(todo_list_file)
-    for row in todo_list:
-        print(row["task_name"])
+import csv
+import time
     
-    fieldnames = ["task_name", "rating", "body_text"] # can this be made dynamic by reading from the file?
-    writer = csv.DictWriter(todo_list_file, delimiter=",", escapechar="\n")
+def headerNames(): # returns the names of the headers
+    for row in reader:
+        return row # breaks after one loop
     
-    # writer.writerow({"task_name" : "Code project", "rating" : "4", "body_text" : "stop being lazy and code"})
+def addItem(): # appends an item to the list
+    taskName = input("Task name: ")
+    
+    rating = -1
+    while rating == -1:
+        try:
+            tempRating = int(input("Rating: "))
+            if tempRating < 0:
+                raise Exception("Positive integers only.") # try to fold this into valueerror?
+            rating = tempRating
+        except ValueError:
+            print("Input is not a positive integer.")    
+    
+    descripton = input("Description: ")
+    
+    writer.writerow([taskName, rating, descripton])
+    
+def printList(): # prints the list
+    print("\nTodo list:\n---------")
+    for row in list(reader)[1:]: # skipping first row
+        starCount = "★" * int(row[1]) # formatting the stars
+        print(f"{starCount} | {row[0]} | {row[2]}")
+    print("")
+
+try:
+    with open("todo.csv", mode='r+', newline='') as todo_list_file:
+        reader = csv.reader(todo_list_file)
+        writer = csv.writer(todo_list_file)
+
+        printList()
+        addItem()
+        # while True:
+        #     addItem(writer)
+        
+except KeyboardInterrupt:
+    print("Closing program.")
